9ebf9d6f3f798c5cf90d3e6a14b9ec70ab4db69d
Zhenbo Li <litimetal@gmail.com>
2018-12-11 10:05:35 -0500

    Handle question type
    Signed-off-by: Zhenbo Li <litimetal@gmail.com>

--- xlsx2json.py
+++ xlsx2json.py
@@ -1,17 +1,41 @@
 import pandas as pd
+import json
 
 file = 'demo.xlsx'
 
 df = pd.read_excel(file, sheet_name=0)
 
+QUESTION_TYPES = ('choice', 'branch', 'fin')
+
+def decideType(qID):
+    if (qID.find('fin') != -1):
+        return ('fin',0)
+    if (qID.find('branch') != -1):
+        return ('branch',1)
+    limit = 1
+    if (qID.find('multi') != -1):
+        import re
+        x = re.search('<(\d+)>', qID)
+        limit = int(x.group(1))
+    return ('choice', limit)
+
 def row2json(df):
     qID      = df[0]
     qDesc    = df[1]
+    (qType,qLimit)    = decideType(qID)
+    assert(qType in QUESTION_TYPES)
     choices  = []
     for col in df[2:]:
         if (pd.isna(col)):   break
         choices.append(col)
-    print(choices)
+    data = {
+        'ID':qID,
+        'Desc':qDesc,
+        'Type':qType,
+        'Answers':qLimit,
+    }
+    json_data = json.dumps(data, ensure_ascii=False)
+    print(json_data)
     return 1
 
 def writeJson(obj):
